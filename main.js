13d60ad581120e0bff2ddccb6a102d255a9dd89a
Przemek Saja <przemek.saja@gmail.com>
2018-06-15 15:07:11 +0200

    Add files

---
+++ main.js
@@ -0,0 +1,27 @@
+//Countries
+
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+
+$('#search').click(searchCountries); //attaching the request under the button from id search
+
+function searchCountries() {
+  	var countryName = $('#country-name').val(); //we take the value entered by the user and assign it to the variable
+  	if(!countryName.length) countryName = 'Poland'; //add a condition to our function to check if the text field is empty
+  	$.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+  	fail(function () {
+      $('#countries').text('No results for your request!');
+    });
+}
+
+function showCountriesList(resp) {
+    countriesList.empty();
+    resp.forEach(function(item) {
+        $('<li>').text(item.name).appendTo(countriesList);
+    })
+}
+
